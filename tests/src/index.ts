6467d5aa9942dcd7d215f2ee1eca31c40589a3d6
Xavier Mitault <xavier.mitault@epitech.eu>
2023-12-26 23:40:05 +0100

    Add a second message to test if it is ok

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,20 +1,24 @@
-import { basicTest, BasicTest, Message } from "pulumi-dynamic-provider-aleph";
+import { Message } from "pulumi-dynamic-provider-aleph";
 import { ItemType } from "aleph-sdk-ts/dist/messages/types";
 import * as pulumi from "@pulumi/pulumi";
 
 
-console.log("test!");
-basicTest();
-
-export const basicRes = new BasicTest("abc", { foo: "barbaz"});
-export const basicResLenName = basicRes.lenName;
-
 export const message = new Message("first-message", {
     content: {
-        body: "hello from pulumi - edit - edit too"
+        body: "hello from pulumi - edit - edit too - but one more is fine ?"
     },
     postType: "pulumi-test-type",
     channel: "pulumi-test-channel",
     storageEngine: ItemType.inline,
 });
 export const alephExporerUrl = message.aleph_explorer_url;
+
+export const message2 = new Message("second-message", {
+    content: {
+        body: "second test just here"
+    },
+    postType: "pulumi-test-type",
+    channel: "pulumi-test-channel",
+    storageEngine: ItemType.inline,
+})
+export const alephExporerUrl2 = message2.aleph_explorer_url;
