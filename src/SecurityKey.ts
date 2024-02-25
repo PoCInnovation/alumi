ac6ae0cb354f83ea9d9dc1325a3a4e40176fda82
Xavier Mitault <xavier.mitault@epitech.eu>
2024-02-25 17:41:26 +0100

    Fix some exports

--- src/SecurityKey.ts
+++ src/SecurityKey.ts
@@ -1,12 +1,5 @@
-import { Aggregate } from './Aggregate'
+import { Aggregate } from './Aggregate';
 import { ItemType } from 'aleph-sdk-ts/dist/messages/types/base';
-
-export type JSONValue =
-  | string
-  | number
-  | boolean
-  | { [x: string]: JSONValue }
-  | Array<JSONValue>;
 
 export type SecurityKeyTypes = 'POST' | 'AGGREGATE' | 'STORE';
 
@@ -21,40 +14,38 @@ export interface SecurityKeyConf {
 
 export const securityKey = (name: string, conf: SecurityKeyConf) => {
   const content: {
-    'authorizations': Array<
-      {
-        'address': string,
-        'types': SecurityKeyTypes[],
-        'post_types'?: string[],
-        'aggregate_keys'?: string[],
-        'chains'?: string[],
-        'channels'?: string[],
-      }
-    >
+    authorizations: Array<{
+      address: string;
+      types: SecurityKeyTypes[];
+      post_types?: string[];
+      aggregate_keys?: string[];
+      chains?: string[];
+      channels?: string[];
+    }>;
   } = {
-    'authorizations': [
+    authorizations: [
       {
-        'address': conf.address,
-        'types': conf.types,
-      }
-    ]
+        address: conf.address,
+        types: conf.types,
+      },
+    ],
   };
   if (conf.postTypes.length != 0) {
-    content.authorizations[0].post_types = conf.postTypes
+    content.authorizations[0].post_types = conf.postTypes;
   }
   if (conf.aggregateKeys.length != 0) {
-    content.authorizations[0].aggregate_keys = conf.aggregateKeys
+    content.authorizations[0].aggregate_keys = conf.aggregateKeys;
   }
   if (conf.chains.length != 0) {
-    content.authorizations[0].chains = conf.chains
+    content.authorizations[0].chains = conf.chains;
   }
   if (conf.channels.length != 0) {
-    content.authorizations[0].channels = conf.channels
+    content.authorizations[0].channels = conf.channels;
   }
   return new Aggregate(name, {
     key: name,
     content: content,
     channel: 'security',
     storageEngine: ItemType.inline,
-  })
-}
+  });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { BasicTestInputs, BasicTest } from './BasicTest';
 export { BasicTestInputs, BasicTest };
 
-import { PostInputs, Post } from './Post';
-export { PostInputs, Post };
+import { PostInputs, Post, PostProviderOutputs } from './Post';
+export { PostInputs, Post, PostProviderOutputs };
 
 import { AggregateInputs, Aggregate } from './Aggregate';
 export { AggregateInputs, Aggregate };
+
+import { securityKey, SecurityKeyConf, SecurityKeyTypes } from './SecurityKey';
+export { securityKey, SecurityKeyConf, SecurityKeyTypes };
